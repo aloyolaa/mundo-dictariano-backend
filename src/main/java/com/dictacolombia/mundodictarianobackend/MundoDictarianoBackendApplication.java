a7eac484a4275a069608d9d54875635043bfe9c3
Alan Loyola <alanloyola2002@gmail.com>
2023-10-19 15:54:39 -0500

    Dependencias y Conexion a Base de Datos agregados

---
+++ src/main/java/com/dictacolombia/mundodictarianobackend/MundoDictarianoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dictacolombia.mundodictarianobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundoDictarianoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundoDictarianoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dictacolombia/mundodictarianobackend/MundoDictarianoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dictacolombia.mundodictarianobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundoDictarianoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
